4640a1d3bb58b4eb4e531fa2e7d41e77d2b41fe6
lafixier <100778884+lafixier@users.noreply.github.com>
2024-07-27 22:28:39 +0900

    feat: add cli commands

--- src/c3k.nim
+++ src/c3k.nim
@@ -1,4 +1,7 @@
 import
+  os
+
+import
   m17n,
   messages,
   types,
@@ -6,20 +9,35 @@ import
   c3k/scan_result
 
 
-proc scan() =
+let c3kDirPath = getConfigDir() / "c3k"
+let settingFilePath = c3kDirPath / "c3k.setting.yaml"
+
+
+proc initCommand(args: seq[string]) =
+  if not c3kDirPath.dirExists:
+    createDir c3kDirPath
+  if not settingFilePath.fileExists:
+    let settingFile = open(settingFilePath, fmWrite)
+    defer: settingFile.close()
+    settingFile.write("")
+
+
+proc scanCommand(args: seq[string]) =
   let m17nEcho = m17nEcho(ja_JP)
   let settingFilePath = "src/.c3k.yaml"
   m17nEcho multiLangMessages.usingXAsASettingFile(settingFilePath)
   m17nEcho multiLangMessages.loadingAndParsingSettingFile
   let settingYaml = loadYaml(settingFilePath)
-  echo settingYaml
   let setting = parseSettingsYaml(settingYaml)
   let scanResult = scan(setting, proc()=discard)
-  echo scanResult
   m17nEcho multiLangMessages.scanFinishedSuccessfuly
-  #m17nEcho multiLangMessages.scanResult(scanResult)
+  # m17nEcho multiLangMessages.scanResult(scanResult)
   echo scanResult.format
 
 
 when isMainModule:
-  scan()
+  import cligen
+  dispatchMulti(
+    [initCommand, cmdName="init"],
+    [scanCommand, cmdName="scan"],
+  )
