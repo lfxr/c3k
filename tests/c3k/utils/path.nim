d568506377d63f2ed590f12b9a47a719bbde8ff6
lafixier <100778884+lfxr@users.noreply.github.com>
2025-01-01 23:04:34 +0900

    test(unexpand_tilde): move

--- tests/c3k/utils/path.nim
+++ tests/c3k/utils/path.nim
@@ -2,9 +2,10 @@ import
   unittest
 
 import
-  ../../../src/c3k/parse_setting {.all.}
+  ../../../src/c3k/utils/path {.all.}
 
 
+# unexpandTilde(path, homeDirPath, dirSep)
 check unexpandTilde("/home/user", "/home/user/", '/') == "~/"
 check unexpandTilde("/home/user/", "/home/user/", '/') == "~/"
 check unexpandTilde("/home/user/home/user", "/home/user/", '/') == "~/home/user"
