5b0127cbdefdaa41addbd121d11bdddd371ee0b9
lafixier <100778884+lfxr@users.noreply.github.com>
2025-03-24 18:30:04 +0900

    feat(ignore): support item kind

--- src/c3k/utils/ignore.nim
+++ src/c3k/utils/ignore.nim
@@ -14,10 +14,13 @@ type IgnorePatternType = enum
 type IgnorePattern = object
   patternType: IgnorePatternType
   raw, normalized: string
+  targetItemKinds: seq[ItemKind]
   isNegation: bool
   appliedFrom: string
 
-type IgnorePredicate = proc (path, workingDir: string): bool {.noSideEffect.}
+type IgnorePredicate = proc (
+    itemPath: string, itemKind: ItemKind, workingDir: string
+): bool {.noSideEffect.}
 
 
 func normalize(rawPattern: string): string =
@@ -27,11 +30,19 @@ func normalize(rawPattern: string): string =
     rawPattern
 
 
+func targetItemKinds(rawPattern: string): seq[ItemKind] =
+  if rawPattern.endsWith("/"):
+    @[dir]
+  else:
+    @[file, dir]
+
+
 func newIgnorePattern(rawPattern: string, appliedFrom: string): ref IgnorePattern =
   result = new IgnorePattern
   result.raw = rawPattern
   # TODO: Implement pattern normalization
   result.normalized = rawPattern.normalize
+  result.targetItemKinds = rawPattern.targetItemKinds
   result.isNegation = rawPattern.startsWith("!")
   result.appliedFrom = appliedFrom
 
@@ -47,10 +58,12 @@ func newIgnorePattern(rawPattern: string, appliedFrom: string): ref IgnorePatter
 
 
 func ignorePredicate(patterns: seq[ref IgnorePattern]): IgnorePredicate =
-  func (path, workingDir: string): bool =
+  func (itemPath: string, itemKind: ItemKind, workingDir: string): bool =
     result = false
     for pattern in patterns:
-      if pattern.normalized == path:
+      if itemKind notin pattern.targetItemKinds:
+        continue
+      if pattern.normalized == itemPath:
         result = not pattern.isNegation
 
 
@@ -61,4 +74,4 @@ when isMainModule:
       newIgnorePattern("!/desktop.ini", ""),
     ]
     predicate: IgnorePredicate = ignorePredicate(patterns)
-  doAssert not predicate("/desktop.ini", "")
+  doAssert not predicate("/desktop.ini", file, "")
